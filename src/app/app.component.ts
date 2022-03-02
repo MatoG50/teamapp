25820472f28300ead0ee241edb24b134ebfb672f
MatoG50 <matog50@gmail.com>
2022-03-02 20:30:41 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+newMemberName ='';
+members:string[]=[];
+errorMessage=''
+addMember(){
+
+if(!this.newMemberName){
+  this.errorMessage="Name can't be Blank"
+  return
+}
+
+ this.members.push(this.newMemberName)
+ this.newMemberName=''
+ this.errorMessage=''
+}
+onInput(member:string){
+this.newMemberName = member;
+this.errorMessage=''
+}
+}
